9d389c37591284d655a275f705ae31291979dea6
mdignan <maxdignan@gmail.com>
2018-05-29 19:37:38 -0400

    is in ride update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html  # index.html.erb
-      format.json  { render :json => {in_ride: @user.in_ride?, ride_id: @user.current_ride } }
+      format.json  { render :json => {in_ride: @user.in_ride?, ride_id: @user.current_ride.id } }
     end
   end
 
