76d950563379db16b49cc3c8903639e0ed02a983
mdignan <maxdignan@gmail.com>
2018-06-04 12:02:43 -0400

    fix

--- db/migrate/20180603230002_create_promo_redemptions.rb
+++ db/migrate/20180603230002_create_promo_redemptions.rb
@@ -1,8 +1,8 @@
 class CreatePromoRedemptions < ActiveRecord::Migration
   def change
     create_table :promo_redemptions do |t|
-      t.references :invited_user
-      t.references :invite_sender_user
+      t.integer :invited_user_id
+      t.integer :invite_sender_user_id
       t.datetime :invited_redeem_time
       t.datetime :invite_sender_redeem_time
 
