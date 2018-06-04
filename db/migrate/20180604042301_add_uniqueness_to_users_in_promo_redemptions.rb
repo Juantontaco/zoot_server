cdadb9be98bd6ef5fe2d3a4c2bd5b6e716d07c0d
mdignan <maxdignan@gmail.com>
2018-06-04 12:06:44 -0400

    index name too long

--- db/migrate/20180604042301_add_uniqueness_to_users_in_promo_redemptions.rb
+++ db/migrate/20180604042301_add_uniqueness_to_users_in_promo_redemptions.rb
@@ -1,5 +1,5 @@
 class AddUniquenessToUsersInPromoRedemptions < ActiveRecord::Migration
   def change
-    add_index :promo_redemptions, [:invited_user_id, :invite_sender_user_id], unique: true
+    add_index :promo_redemptions, [:invited_user_id, :invite_sender_user_id], unique: true, name: 'uniq_promo_index'
   end
 end
