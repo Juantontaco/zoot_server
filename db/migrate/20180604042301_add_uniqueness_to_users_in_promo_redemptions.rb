fcb5544a4da5f95ee920b464eca0f2b2d53961fa
mdignan <maxdignan@gmail.com>
2018-06-04 12:04:29 -0400

    fix indec

--- db/migrate/20180604042301_add_uniqueness_to_users_in_promo_redemptions.rb
+++ db/migrate/20180604042301_add_uniqueness_to_users_in_promo_redemptions.rb
@@ -1,5 +1,5 @@
 class AddUniquenessToUsersInPromoRedemptions < ActiveRecord::Migration
   def change
-    add_index :promo_redemptions, [:invited_user, :invite_sender_user], unique: true
+    add_index :promo_redemptions, [:invited_user_id, :invite_sender_user_id], unique: true
   end
 end
