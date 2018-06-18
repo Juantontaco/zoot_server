a3eb58cdfc64d50d41c9c112a318402f6cbc3824
mdignan <maxdignan@gmail.com>
2018-06-17 22:50:05 -0400

    fix check if in ride under condition that they are not in ride

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,16 @@ class UsersController < ApplicationController
   def check_if_in_ride
     @user = current_user
 
+    in_ride = @user.in_ride?
+    ride_id = nil
+
+    if in_ride
+      ride_id = @user.current_ride.id
+    end
+
     respond_to do |format|
       format.html  # index.html.erb
-      format.json  { render :json => {in_ride: @user.in_ride?, ride_id: @user.current_ride.id } }
+      format.json  { render :json => {in_ride: in_ride, ride_id: ride_id } }
     end
   end
 
